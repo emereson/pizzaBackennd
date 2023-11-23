a2dd4c52c449f595cbb141df91f97ebe24065b25
emereson <escanorrosa123@gmail.com>
2023-11-23 14:35:22 -0500

    hola

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-import path from "path";
-import cors from 'cors'
+import cors from 'cors';
 import paymentRoutes from './payment.routes.js';
 import dotenv from 'dotenv';
 
 const app = express();
+
 dotenv.config();
 app.use(morgan("dev"));
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.use(paymentRoutes);
-app.use(express.static(path.resolve("client")));
+app.use('/api/v1', paymentRoutes);
 
-app.listen(3010);
-console.log("Server on port", 3010);
+app.listen(3010, () => {
+  console.log("Server on port", 3010);
+});
