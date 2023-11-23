5edcc5ff27d972c0ce5889417df27387ccaf531c
emereson <escanorrosa123@gmail.com>
2023-11-23 15:19:19 -0500

    hola

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const express = require ("express")
-const morgan = require ("morgan")
-const cors = require ('cors')
-const paymentRoutes = require ('./payment.routes.js')
 require('dotenv').config();
+const app = require('./app');
 
 
-const app = express();
+const port = +process.env.PORT || 3024;
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/v1', paymentRoutes);
-
-app.listen(3010, () => {
-  console.log("Server on port", 3010);
+app.listen(port, () => {
+  console.log(`App Running on port ${port}`);
 });
